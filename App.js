34165dcc60085502124158b3c1bdd1da3f1f1e16
Rian <alvesrianpf@gmail.com>
2023-09-11 08:48:04 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default async function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [valueQR, setValueQR] = useState("");
+  // const 
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraOpen, setCameraOpen] = useState(false);
+
+  console.log(permission, requestPermission);
+  // requestPermission()
+  console.log(permission, requestPermission);
+  // if:
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {cameraOpen && 
+         <Camera style={styles.camera} type={type} onBarCodeScanned={(e) => {
+          setValueQR(e.data);
+          setCameraOpen(false);
+          return;
+        }}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>}
+        {!cameraOpen && 
+          <View>
+            <Button title="Ler Codigo QRcode" onPress={() => setCameraOpen(true)}/>  
+            {valueQR.length > 0 && 
+              <Text>O texto em seu QRcode é: {valueQR}</Text>}
+          </View>}
     </View>
   );
 }
@@ -13,8 +57,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    // flex: 1,
+    marginHorizontal: "auto",
+    borderWidth: 1,
+    borderColor: "#d3d3d3",
+    height: 500,
+    width: "100%"
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
